d6b520c041fc8268e71a8f164effc1a0d1ef3866
Learncin9 <driveg808@gmail.com>
2022-09-08 19:05:57 +0900

    2022/09/08 hotfix

--- js/setting.js
+++ js/setting.js
@@ -1,28 +1,30 @@
 class ElementData {
-    constructor(query, ani_class) {
+    constructor(query, ani_class, callback) {
         this.element = document.querySelector(query);
         this.animation_class = ani_class;
+        this.callback = callback;
     }
 }
 
 export const OBSERVE_DATAs = [
-    new ElementData(".mc-server-intro", "default-fade-in", (elem) => 
-    setTimeout(() => {
-        const stable_panel = elem.querySelector("div div");
-        stable_panel.classList.add("server-stable-chart");
-
+    new ElementData(".mc-server-intro", "default-fade-in", (elem) => {
         setTimeout(() => {
-            stable_panel.querySelectorAll("h3").forEach(elem => {
-                elem.style.display = 'block'
-                elem.classList.add("default-fade-in")
-            });
-            
-            stable_panel.querySelectorAll("div").forEach(elem => {
-                elem.style.display = 'block'
-                elem.classList.add("default-fade-in")
-            });
-        }, 200);
-    }, 300))
+            const stable_panel = elem.querySelector("div div");
+            stable_panel.classList.add("server-stable-chart");
+    
+            setTimeout(() => {
+                stable_panel.querySelectorAll("h3").forEach(elem => {
+                    elem.style.display = 'block';
+                    elem.classList.add("default-fade-in");
+                });
+                
+                stable_panel.querySelectorAll("div").forEach(elem => {
+                    elem.style.display = 'block';
+                    elem.classList.add("default-fade-in");
+                });
+            }, 200);
+        }, 300)
+    })
 ];
 
 
